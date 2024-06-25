2218e200c61ec70a7ca2e12772da204de34d6825
MP <MP@DESKTOP-1TUT2A4>
2024-06-25 18:50:05 +0530

    first commit

---
+++ b96corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+//Program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10;
+	String name;
+	int age;
+	
+	void show()
+	{
+		System.out.println("serialnumber"+serialNum+"\t Name"+name+"\t age" +age);
+	}
+	
+}
