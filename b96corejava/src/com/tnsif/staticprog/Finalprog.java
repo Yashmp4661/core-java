677c31a7ae18c9789ee3f06bd43a0154545b7f5a
MP <MP@DESKTOP-1TUT2A4>
2024-09-20 14:06:22 +0530

    commit

---
+++ b96corejava/src/com/tnsif/staticprog/Finalprog.java
@@ -0,0 +1,14 @@
+package com.tnsif.staticprog;
+
+public class Finalprog {
+	final int a=10;
+	void display() {
+		System.out.println("The value: "+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
